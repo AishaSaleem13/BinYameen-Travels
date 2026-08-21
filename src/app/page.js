80db45878820d36d846abad059350abb82dabc06
AishaSaleem13 <aishasaleem1308@gmail.com>
2026-08-22 04:40:19 +0500

    done

--- src/app/page.js
+++ src/app/page.js
@@ -1,38 +1,30 @@
-
-// import Navbar from "@/components/Navbar";
-
-import ContactUs from "@/components/Contact";
+import dynamic from "next/dynamic";
 import Heronew from "@/components/Heronew";
-import UmrahIntroSection from "@/components/Umrah";
+import BookingWidget from "@/components/formbooking";
 import About from "@/components/about";
-import Hero from "@/components/about2";
-import ValuedPilgrims from "@/components/clientreview";
-import FooterSection from "@/components/footer";
-import BookingWidget from "@/components/formbooking"
-import ServicesFeatures from "@/components/googlereviews";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-import CuratedActivities from "@/components/preview2";
-import WhyChooseUs from "@/components/whychooseus";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+// Below-the-fold sections: don't block initial hydration
+const CuratedActivities = dynamic(() => import("@/components/preview2"));
+const WhyChooseUs = dynamic(() => import("@/components/whychooseus"));
+const ServicesFeatures = dynamic(() => import("@/components/googlereviews"));
+const ValuedPilgrims = dynamic(() => import("@/components/clientreview"));
+const ContactUs = dynamic(() => import("@/components/Contact"));
+const FooterSection = dynamic(() => import("@/components/footer"));
+
 export default function Home() {
   return (
-  <>
-  
-     <div className="bg-white">
-       <Heronew/>
-  <BookingWidget/>
-  <About/>
-  <CuratedActivities/>
-<WhyChooseUs/>
-<ServicesFeatures/>
-<ValuedPilgrims/>
-<ContactUs/>
-<FooterSection/>
-
-     </div>
-    
-{/* <HeroSlider/> */}
- 
-  </>
+    <div className="bg-white">
+      <Heronew />
+      <BookingWidget />
+      <About />
+      <CuratedActivities />
+      <WhyChooseUs />
+      <ServicesFeatures />
+      <ValuedPilgrims />
+      <ContactUs />
+      <FooterSection />
+      <SpeedInsights />
+    </div>
   );
 }
